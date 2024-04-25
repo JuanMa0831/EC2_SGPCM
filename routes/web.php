90579654ec426989ca5072201c16c1ffbbfd3589
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-04-24 22:50:52 -0500

    Rutas_Gall

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/paciente/create', [PacienteController::class, 'create'])->name('pacientes.create');;
+// Rutas para la gestión de pacientes
+Route::get('/pacientes', [PacienteController::class, 'index'])->name('pacientes.index');
+Route::get('/pacientes/create', [PacienteController::class, 'create'])->name('pacientes.create');
+Route::post('/pacientes', [PacienteController::class, 'store'])->name('pacientes.store');
+Route::get('/pacientes/{paciente}', [PacienteController::class, 'show'])->name('pacientes.show');
+Route::get('/pacientes/{paciente}/edit', [PacienteController::class, 'edit'])->name('pacientes.edit');
+Route::put('/pacientes/{paciente}', [PacienteController::class, 'update'])->name('pacientes.update');
+Route::delete('/pacientes/{paciente}', [PacienteController::class, 'destroy'])->name('pacientes.destroy');
