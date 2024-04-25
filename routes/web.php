49de5af8e3d69213a0dc3594d83b6f2913a9dab5
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-04-24 22:46:20 -0500

    Rutas_P1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PacienteController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/paciente/create', [PacienteController::class, 'create'])->name('pacientes.create');;
