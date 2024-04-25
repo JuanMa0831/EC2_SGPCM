c3a7b15bd0f1a5129f6883fd7b14beb7b2f2c688
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-04-24 23:48:24 -0500

    Add_Columns_P.Show

--- resources/views/Paciente/show.blade.php
+++ resources/views/Paciente/show.blade.php
@@ -6,6 +6,11 @@
     <h1>Detalles del Paciente</h1>
     <p><strong>Nombre:</strong> {{ $paciente->nombre }}</p>
     <p><strong>Apellido:</strong> {{ $paciente->apellido }}</p>
+    <p>Fecha de Nacimiento: {{ $paciente->fecha_nacimiento }}</p>
+    <p>Género: {{ $paciente->genero }}</p>
+    <p>Dirección: {{ $paciente->direccion }}</p>
+    <p>Teléfono: {{ $paciente->telefono }}</p>
+    <p>Email: {{ $paciente->email }}</p>
     <!-- Muestra más detalles del paciente según sea necesario -->
     <a href="{{ route('pacientes.index') }}" class="btn btn-secondary">Volver al Listado</a>
 @endsection
