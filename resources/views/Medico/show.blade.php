170716192c4951988253f3e9d358194c9fb2b3f5
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-04-25 19:40:54 -0500

    Vista_M3

---
+++ resources/views/Medico/show.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/medicos/show.blade.php -->
+
+<h1>Detalles del Médico</h1>
+
+<p>Nombre: {{ $medico->nombre }}</p>
+<p>Apellido: {{ $medico->apellido }}</p>
+<!-- Agrega aquí los demás campos -->
