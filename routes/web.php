44ce9ed6154425f8701e6a9426b208fc270b16e0
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-04-25 19:44:34 -0500

    Rutas_Medico

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\MedicoController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,11 @@ Route::get('/pacientes/{paciente}', [PacienteController::class, 'show'])->name('
 Route::get('/pacientes/{paciente}/edit', [PacienteController::class, 'edit'])->name('pacientes.edit');
 Route::put('/pacientes/{paciente}', [PacienteController::class, 'update'])->name('pacientes.update');
 Route::delete('/pacientes/{paciente}', [PacienteController::class, 'destroy'])->name('pacientes.destroy');
+// Rutas para gestión de médicos
+Route::get('/medicos', [MedicoController::class, 'index'])->name('medicos.index');
+Route::get('/medicos/create', [MedicoController::class, 'create'])->name('medicos.create');
+Route::post('/medicos', [MedicoController::class, 'store'])->name('medicos.store');
+Route::get('/medicos/{medico}', [MedicoController::class, 'show'])->name('medicos.show');
+Route::get('/medicos/{medico}/edit', [MedicoController::class, 'edit'])->name('medicos.edit');
+Route::put('/medicos/{medico}', [MedicoController::class, 'update'])->name('medicos.update');
+Route::delete('/medicos/{medico}', [MedicoController::class, 'destroy'])->name('medicos.destroy');
