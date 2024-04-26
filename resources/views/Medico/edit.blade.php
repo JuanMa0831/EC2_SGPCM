2a3df8544381739081a9a60997b9fcbd663a388a
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-04-25 20:26:06 -0500

    Conf_V4

--- resources/views/Medico/edit.blade.php
+++ resources/views/Medico/edit.blade.php
@@ -1,13 +1,46 @@
 <!-- resources/views/medicos/edit.blade.php -->
 
-<h1>Editar Médico</h1>
+<@extends('layouts.app')
 
-<!-- Formulario para editar un médico existente -->
-<form method="POST" action="{{ route('medicos.update', $medico->id) }}">
-    @csrf
-    @method('PUT')
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" value="{{ $medico->nombre }}">
-    <!-- Agrega aquí los campos restantes del formulario -->
-    <button type="submit">Actualizar</button>
-</form>
+@section('content')
+    <h1>Editar Paciente</h1>
+    <!-- Formulario de edición de pacientes -->
+    <form action="{{ route('pacientes.update', $paciente) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" class="form-control" value="{{ $medico->nombre }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="apellido">Apellido:</label>
+            <input type="text" name="apellido" class="form-control" value="{{ $medico->apellido }}" required>
+        </div>
+
+        <div class="form-group">
+        <label for="especialidad">Especialidad:</label>
+        <input type="date" name="especialidad" class="form-control" value="{{ $medico->especialidad }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="horario">Horarios:</label>
+            <input type="text" name="horario" class="form-control" value="{{ $medico->horarios }}">
+        </div>
+
+        <div class="form-group">
+            <label for="telefono">Teléfono:</label>
+            <input type="text" name="telefono" class="form-control" value="{{ $medico->telefono }}">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" name="email" class="form-control" value="{{ $medico->email }}">
+        </div>
+
+        <!-- Agrega más campos del formulario según sea necesario -->
+        <button type="submit" class="btn btn-primary">Actualizar Paciente</button>
+        <a href="{{ route('pacientes.show', $paciente->id) }}" class="btn btn-primary">Cancelar</a>
+    </form>
+@endsection
+
